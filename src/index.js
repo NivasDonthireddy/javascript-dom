87c2e7bc232fca596a2a765058c3a9a6a63ec176
Nivas Donthireddy <dlnivas@hotmail.com>
2022-11-06 16:26:24 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import '../assets/css/style.css';
-
 const app = document.getElementById('app');
-app.innerHTML = `
-  <h1>JavaScript DOM</h1>
+  app.innerHTML = `
+  <h1>JavaScript DOM</h1>  
 `;
+
+console.log(document);
+console.dir(document);
+
+console.log(document.head.nodeType);
+console.dir(Node);
